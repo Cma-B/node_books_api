04c70be4278282609c301fe83b00f7ffe05be0c0
Sima <sima.boreiri@gmail.com>
2020-08-26 15:09:22 +0200

    database migration and creates the seed file and models for books

---
+++ database/seeders/20200826124548-Book.js
@@ -0,0 +1,24 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "Books",
+      [
+        {
+          title: "Intro",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "Intro C",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("Books", null, {});
+  },
+};
