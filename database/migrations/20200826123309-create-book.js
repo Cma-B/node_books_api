e1061e53d5e7ac18cb4160c258a95f490ea5676e
Sima <sima.boreiri@gmail.com>
2020-08-26 23:38:57 +0200

    writing third test to show the book title by id and adds show action to controller and define route for it and all test passed

--- database/migrations/20200826123309-create-book.js
+++ database/migrations/20200826123309-create-book.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulk('Books', {
+    await queryInterface.createTable('Books', {
       id: {
         allowNull: false,
         autoIncrement: true,
--- database/seeders/20200826124548-Book.js
+++ database/seeders/20200826124548-Book.js
@@ -18,6 +18,7 @@ module.exports = {
       {}
     );
   },
+  
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete("Books", null, {});
   },
