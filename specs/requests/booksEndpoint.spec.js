5348318c79d7907c8097b6bdeffabf216e1406d0
Sima <sima.boreiri@gmail.com>
2020-08-28 15:24:00 +0200

    adds test for showing the author of the books and adds factory girl for author with attributes and modifies the show action controllers

--- specs/requests/booksEndpoint.spec.js
+++ specs/requests/booksEndpoint.spec.js
@@ -1,7 +1,7 @@
 const app = require("../../app");
 const supertest = require("supertest");
 const expect = require("chai").expect;
-const { factory } = require('../helpers')
+const { factory } = require("../helpers");
 
 let server, request, response;
 
@@ -15,16 +15,29 @@ after((done) => {
 });
 
 beforeEach(async () => {
-  await factory.createMany('Book', 2, [
-    { id: 100, title: "this is the first one from factory" },
-    { id: 101, title: "this is the second  one from factory" }
+  const author = await factory.create("Author", {
+    id: 1,
+    firstName: "Adi",
+    lastName: "Naik",
+  });
 
-  ])
-})
+  await factory.createMany("Book", 2, [
+    {
+      id: 100,
+      title: "this is the first one from factory",
+      AuthorId: author.id,
+    },
+    {
+      id: 101,
+      title: "this is the second  one from factory",
+      AuthorId: author.id,
+    },
+  ]);
+});
 
 afterEach(async () => {
-  await factory.cleanUp()
-})
+  await factory.cleanUp();
+});
 
 describe("GET /api/v1/books", () => {
   beforeEach(async () => {
@@ -36,13 +49,22 @@ describe("GET /api/v1/books", () => {
   });
 
   it("is expected to respond with list of two books", () => {
-    expect(response.body['books'].length).to.equal(2);
+    expect(response.body["books"].length).to.equal(2);
   });
 });
 
-describe('GET /api/v1/books/:id', () => {
-  it("is expected to responsd with a single book", async () => {
-    response = await request.get('/api/v1/books/101')
-    expect(response.body.book.title).to.equal('this is the second  one from factory')
-  })
-})
+describe("GET /api/v1/books/:id", () => {
+  beforeEach(async () => {
+    response = await request.get("/api/v1/books/101");
+  });
+
+  it("is expected to responsd with a single book by title ", async () => {
+    expect(response.body.book.title).to.equal(
+      "this is the second  one from factory"
+    );
+  });
+
+  it("is expected to responsd with a single book by author ", async () => {
+    expect(response.body.book.author.firstName).to.equal("Adi");
+  });
+});
